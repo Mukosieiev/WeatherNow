7644cc93b9576f03da991e76de6f36d03d24e651
Mooteron <roman.mukosieiev@gmail.com>
2020-09-27 09:26:36 -0400

    Rewrote the front end. Added graphic icons for the weather. Major fixes in back end. Added additional temperature measurements. Min/max temperature, etc. Minor fixes in the file structure

--- src/main/java/com/ib/weather/AppController.java
+++ src/main/java/com/ib/weather/AppController.java
@@ -7,26 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+// simple comment
 
 @Controller
 public class AppController {
 
-	@Value("${api.key}")
-	private String ApiKey;
-	
-	@RequestMapping("/")
-	public String getHome() {
-		return "index.jsp";
-	}
-	
-	@RequestMapping("/getTemp")
-	@ResponseBody
-	public ModelAndView getCityTemperature(String cityname) {
-		ModelAndView MV = new ModelAndView("weather.jsp");
-		RestTemplate restTemplate = new RestTemplate();
-		WeatherData w = restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + ApiKey, WeatherData.class);
-		MV.addObject(w);
-		return MV;
-	}
-	
+    @Value("${api.key}")
+    private String ApiKey;
+
+    @RequestMapping("/")
+    public String getHome() {
+        return "index.jsp";
+    }
+
+    @RequestMapping("/getTemp")
+    @ResponseBody
+    public ModelAndView getCityTemperature(String cityname) {
+        ModelAndView MV = new ModelAndView("weather.jsp");
+        RestTemplate restTemplate = new RestTemplate();
+        WeatherData w = restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + ApiKey, WeatherData.class);
+        MV.addObject(w);
+        return MV;
+    }
+
 }
